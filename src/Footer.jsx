5324552156b3553d4bbab4564cdd8c199365cbf4
David Duran <dd@Davids-MBP.fios-router.home>
2023-11-21 02:03:03 -0500

    update-links

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -4,30 +4,13 @@ const Footer = () => {
       <footer className="bg-gray-900 text-white p-4">
         <div className="flex justify-center space-x-4">
           <a
-            href="https://github.com/yourusername"
+            href="https://github.com/ddurandev"
             target="_blank"
             rel="noopener noreferrer"
             className="hover:text-gray-300"
           >
             GitHub
           </a>
-          <a
-            href="https://linkedin.com/in/yourprofile"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-gray-300"
-          >
-            LinkedIn
-          </a>
-          <a
-            href="https://stackoverflow.com/users/yourid"
-            target="_blank"
-            rel="noopener noreferrer"
-            className="hover:text-gray-300"
-          >
-            Stack Overflow
-          </a>
-          {/* Add other social platform links as needed */}
         </div>
       </footer>
     );
--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -10,21 +10,21 @@ const Portfolio = () => {
       <div className="grid grid-cols-2 gap-4">
         {/* First project */}
         <div className="text-center">
-          <a href="https://example.com/app1">
+          <a href="https://arcane-refuge-32686-92d3868f1b08.herokuapp.com/">
             <img src={Musik} alt="App 1" className="h-48 w-full object-contain mb-2" />
-            <p className="text-blue-500 hover:underline">App 1</p>
+            <p className="text-blue-500 hover:underline">Musik App</p>
           </a>
-          <a href="https://github.com/username/repo1">
+          <a href="https://github.com/HunterMuratore/Musik">
             <p className="text-gray-500 hover:underline">GitHub Repo</p>
           </a>
         </div>
         {/* Second project */}
         <div className="text-center">
-          <a href="https://example.com/app2">
+          <a href="https://apohl53.github.io/Workout_Planner_Project/">
             <img src={Workout} alt="App 2" className="h-48 w-full object-contain mb-2" />
-            <p className="text-blue-500 hover:underline">App 2</p>
+            <p className="text-blue-500 hover:underline">Work Out Planner</p>
           </a>
-          <a href="https://github.com/username/repo2">
+          <a href="https://github.com/apohl53/Workout_Planner_Project">
             <p className="text-gray-500 hover:underline">GitHub Repo</p>
           </a>
         </div>
